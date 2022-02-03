9bd207b36ad509d7431ac05d257b087dd3f0d3b1
gorbatkoff <gorbatkovartyom@gmail.com>
2022-02-03 15:28:15 +0300

    Add comments

--- routes/read.js
+++ routes/read.js
@@ -1,35 +1,35 @@
 // const express = require('express');
-const router = require('express').Router();
-const { readDataBase, filter } = require('../helper');
-const { query, validationResult } = require('express-validator');
+const router = require('express').Router(); // import routes
+const { readDataBase, filter } = require('../helper'); // reading of database
+const { query, validationResult } = require('express-validator'); // import express-validotr
 
-router.get('/tasks',
-    query('filterBy')
-        .isIn(['', 'done', 'undone'])
-        .withMessage('filterBy should include all, done or undone'),
+router.get('/tasks', // getting array of tassk
+    query('filterBy') // validator
+        .isIn(['', 'done', 'undone']) // validator
+        .withMessage('filterBy should include all, done or undone'), //message if we have errpr
     (req, res) => {
         try {
-            const errors = validationResult(req);
+            const errors = validationResult(req); // create array of errors
 
-            if (!errors.isEmpty()) {
+            if (!errors.isEmpty()) { // if array of errors isn't empty return 400
                 return res.status(400);
             }
 
-            let todos = readDataBase();
+            let todos = readDataBase(); // create array of current tasks
 
-            const params = [
+            const params = [ // add params to array of params
                 req.query.filterBy ?? 'all',
                 req.query.order ?? 'asc',
                 req.query.pp ?? 5,
                 req.query.page ?? 1
             ];
 
-            todos = filter(...params);
-            res.send(todos)
+            todos = filter(...params); // filter array by params
+            res.send(todos) // return array of tasks to front page
         }
 
         catch (e) {
-            return res.sendStatus(400);
+            return res.sendStatus(400); //if we have some problems return 400
         }
 
     })
