5ae001035bb4a4edc92d27b04553eb1532ccdec2
gorbatkoff <gorbatkovartyom@gmail.com>
2022-02-03 13:01:01 +0300

    add cors

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const express = require('express');  // Импорт Express.js
 const {PORT} = require('./config'); // Импорт Порта из конфига
 const app = express(); // Создание объекта приложения
+const cors = require('cors'); // Providing middleware
 
+app.use(cors()); // usage cors middleware
 app.use(express.json()); // Распознование объекта как JSON 
 
 const taskRoute = require('./routes/route.js') // Импорт роутеров
 
 app.use('/', taskRoute); // Маршрутизация
-
-// fs.readFile('./tasks.json', (err, data) => {
-//     if(err) throw err;
-//     data = JSON.parse(data)
-    
-//     console.log(data[1].name);
-// })
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT} port`);} // App listener
