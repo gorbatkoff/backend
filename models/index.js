2983610d80d20c1d25f7b235ab60f9313370ef91
gorbatkoff <gorbatkovartyom@gmail.com>
2022-02-08 19:18:58 +0300

    delete comment

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ const env = process.env.NODE_ENV || 'development';
 // const config = require(__dirname + '/../config/config')[env];
 const db = {};
 
-const { 
+const {
   dialect,
   database,
   username,
@@ -20,13 +20,6 @@ const sequelize = new Sequelize(database, username, password, {
   dialect: dialect,
   host: host,
 });
-
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
 
 fs
   .readdirSync(__dirname)
