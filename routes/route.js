1a0c5e184ba1f957db7c753574dcc562d5645ec9
gorbatkoff <gorbatkovartyom@gmail.com>
2022-02-10 15:24:17 +0300

    Change old paths to new

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = require('express').Router()
 
-router.use('/', require('./read'))
-router.use('/', require('./create'))
-router.use('/', require('./update'))
-router.use('/', require('./delete'))
+router.use('/', require('./todos.get'))
+router.use('/', require('./todo.post'))
+router.use('/', require('./todo.patch'))
+router.use('/', require('./todo.delete'))
 
 module.exports = router;
